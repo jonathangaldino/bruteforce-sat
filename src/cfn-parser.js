4e496fe9394b1807bcb392c926fa59a15f0d8d50
Jonathan <johnzballad@gmail.com>
2020-10-08 03:12:28 -0300

    uncoupling makeClause from cfn-parser

--- src/cfn-parser.js
+++ src/cfn-parser.js
@@ -1,17 +1,15 @@
-import buildMakeClause from "./clause";
-import { splitAndTrim } from './utils';
 
-export default function CNFParser () {
-  const userInput = `(~a OR b) AND (a OR c)`;
+export default function buildMakeCNFParser ({ splitAndTrim, makeClause }) {
+  return function makeCNFParser() {
+    // of course, the use will enter the input later
+    const userInput = `(~a OR b) AND (a OR c)`;
 
-  const makeClause = buildMakeClause({ splitAndTrim }); 
+    const unparsedClauses = splitAndTrim(userInput, 'AND');
 
-  const unparsedClauses = splitAndTrim(userInput, 'AND');
+    const clauses = unparsedClauses.map(clause => makeClause(clause))  
 
-  const clauses = unparsedClauses.map(clause => makeClause(clause))  
-
-  clauses.forEach(clause => console.log(clause.existsNegatedBariables()));
-  
-  return clauses;
+    clauses.forEach(clause => console.log(clause.existsNegatedBariables()));
+    
+    return clauses;
+  }
 }
-
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import CNFParser from './cfn-parser';
+import buildMakeCNFParser from './cfn-parser';
+import buildMakeClause from './clause';
+import { splitAndTrim } from './utils';
+
+const makeClause = buildMakeClause({ splitAndTrim });
+
+const CNFParser = buildMakeCNFParser({ splitAndTrim, makeClause });
 
 const clauses = CNFParser();
